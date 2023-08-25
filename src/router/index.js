8128f662d83c4b262b90b6c9e71d956278234b00
zhouqizhi <zhouqz@yuanqitech.com>
2023-08-25 09:34:24 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,326 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import Layout from '@/views/layout/Layout'
+
+export const constantRouterMap = [
+  {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path*',
+        component: () => import('@/views/redirect/index')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/auth-redirect',
+    component: () => import('@/views/login/authredirect'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/errorPage/404'),
+    hidden: true
+  },
+  {
+    path: '/401',
+    component: () => import('@/views/errorPage/401'),
+    hidden: true
+  },
+  {
+    path: '',
+    component: Layout,
+    redirect: 'dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index'),
+        name: 'Dashboard',
+        meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+      }
+    ]
+  }
+]
+
+export default new Router({
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+
+export const asyncRouterMap = [
+  {
+    path: '/stat',
+    component: Layout,
+    redirect: 'noredirect',
+    alwaysShow: true,
+    name: 'statManage',
+    meta: {
+      title: '统计',
+      icon: 'chart'
+    },
+    children: [
+      {
+        path: 'user',
+        component: () => import('@/views/stat/user'),
+        name: 'statUser',
+        meta: {
+          perms: ['GET /admin/stat/user'],
+          title: '用户统计',
+          icon: 'user',
+          noCache: true
+        }
+      },
+      {
+        path: 'order',
+        component: () => import('@/views/stat/order'),
+        name: 'statOrder',
+        meta: {
+          perms: ['GET /admin/stat/order'],
+          title: '订单统计',
+          icon: 'shopping',
+          noCache: true
+        }
+      },
+      {
+        path: 'goods',
+        component: () => import('@/views/stat/goods'),
+        name: 'statGoods',
+        meta: {
+          perms: ['GET /admin/stat/goods'],
+          title: '商品统计',
+          icon: 'table',
+          noCache: true
+        }
+      }
+    ]
+  },
+  {
+    path: '/user',
+    component: Layout,
+    redirect: 'noredirect',
+    alwaysShow: true,
+    name: 'userManage',
+    meta: {
+      title: '用户管理',
+      icon: 'peoples'
+    },
+    children: [
+      {
+        path: 'user',
+        component: () => import('@/views/user/user'),
+        name: 'user',
+        meta: {
+          perms: ['GET /admin/user/list'],
+          title: '会员管理',
+          icon: 'user',
+          noCache: true
+        }
+      },
+      {
+        path: 'address',
+        component: () => import('@/views/user/address'),
+        name: 'address',
+        meta: {
+          perms: ['GET /admin/address/list'],
+          title: '收货地址',
+          icon: 'email',
+          noCache: true
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/mall',
+    component: Layout,
+    redirect: 'noredirect',
+    alwaysShow: true,
+    name: 'mallManage',
+    meta: {
+      title: '商场管理',
+      icon: 'shopping'
+    },
+    children: [
+      {
+        path: 'region',
+        component: () => import('@/views/mall/region'),
+        name: 'region',
+        meta: {
+          title: '行政区域',
+          icon: 'international',
+          noCache: true
+        }
+      },
+      {
+        path: 'brand',
+        component: () => import('@/views/mall/brand'),
+        name: 'brand',
+        meta: {
+          perms: ['GET /admin/brand/list', 'POST /admin/brand/create', 'GET /admin/brand/read', 'POST /admin/brand/update', 'POST /admin/brand/delete'],
+          title: '品牌制造商',
+          icon: 'example',
+          noCache: true
+        }
+      },
+      {
+        path: 'category',
+        component: () => import('@/views/mall/category'),
+        name: 'category',
+        meta: {
+          perms: ['GET /admin/category/list', 'POST /admin/category/create', 'GET /admin/category/read', 'POST /admin/category/update', 'POST /admin/category/delete'],
+          title: '商品类目',
+          icon: 'list',
+          noCache: true
+        }
+      },
+      {
+        path: 'order',
+        component: () => import('@/views/mall/order'),
+        name: 'order',
+        meta: {
+          perms: ['GET /admin/order/list', 'GET /admin/order/detail', 'POST /admin/order/ordership', 'POST /admin/order/orderrefund', 'POST /admin/order/orderreply'],
+          title: '订单管理',
+          icon: 'nested',
+          noCache: true
+        }
+      },
+      {
+        path: 'keyword',
+        component: () => import('@/views/mall/keyword'),
+        name: 'keyword',
+        meta: {
+          perms: ['GET /admin/keyword/list', 'POST /admin/keyword/create', 'GET /admin/keyword/read', 'POST /admin/keyword/update', 'POST /admin/keyword/delete'],
+          title: '关键词',
+          icon: 'bug',
+          noCache: true
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/goods',
+    component: Layout,
+    redirect: 'noredirect',
+    alwaysShow: true,
+    name: 'goodsManage',
+    meta: {
+      title: '商品管理',
+      icon: 'tab'
+    },
+    children: [
+      {
+        path: 'list',
+        component: () => import('@/views/goods/list'),
+        name: 'goodsList',
+        meta: {
+          perms: ['GET /admin/goods/list', 'POST /admin/goods/delete'],
+          title: '商品列表',
+          icon: 'list',
+          noCache: true
+        }
+      },
+      {
+        path: 'create',
+        component: () => import('@/views/goods/create'),
+        name: 'goodsCreate',
+        meta: {
+          perms: ['POST /admin/goods/create'],
+          title: '商品上架',
+          icon: 'link',
+          noCache: true
+        }
+      },
+      {
+        path: 'edit',
+        component: () => import('@/views/goods/edit'),
+        name: 'goodsEdit',
+        meta: {
+          perms: ['GET /admin/goods/detail', 'POST /admin/goods/update', 'POST /admin/goods/catAndBrand'],
+          title: '商品编辑',
+          icon: 'form',
+          noCache: true
+        },
+        hidden: true
+      },
+      {
+        path: 'comment',
+        component: () => import('@/views/goods/comment'),
+        name: 'goodsComment',
+        meta: {
+          perms: ['GET /admin/comment/list', 'POST /admin/comment/delete'],
+          title: '商品评论',
+          icon: 'message',
+          noCache: true
+        }
+      }
+    ]
+  },
+
+  {
+    path: '/sys',
+    component: Layout,
+    redirect: 'noredirect',
+    alwaysShow: true,
+    name: 'sysManage',
+    meta: {
+      title: '系统管理',
+      icon: 'component'
+    },
+    children: [
+      {
+        path: 'admin',
+        component: () => import('@/views/sys/admin'),
+        name: 'admin',
+        meta: {
+          perms: ['GET /admin/admin/list', 'POST /admin/admin/create', 'POST /admin/admin/update', 'POST /admin/admin/delete'],
+          title: '管理员',
+          icon: 'people',
+          noCache: true
+        }
+      },
+      {
+        path: 'role',
+        component: () => import('@/views/sys/role'),
+        name: 'role',
+        meta: {
+          perms: ['GET /admin/role/list', 'POST /admin/role/create', 'POST /admin/role/update', 'POST /admin/role/delete', 'GET /admin/role/permissions', 'POST /admin/role/permissions'],
+          title: '角色管理',
+          icon: 'qq',
+          noCache: true
+        }
+      }
+    ]
+  },
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: 'noredirect',
+    alwaysShow: true,
+    meta: {
+      title: '设置',
+      icon: 'chart'
+    },
+    children: [
+      {
+        path: 'password',
+        component: () => import('@/views/profile/password'),
+        name: 'password',
+        meta: { title: '修改密码', icon: 'component', noCache: true }
+      }
+    ],
+    hidden: false
+  },
+
+  { path: '*', redirect: '/404', hidden: true }
+]
