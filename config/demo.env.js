997b91fcf33b07194f24dd776918c00814876560
zhouqizhi <zhouqz@yuanqitech.com>
2023-09-03 09:04:08 +0800

    init

--- config/demo.env.js
+++ config/demo.env.js
@@ -1,5 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
   ENV_CONFIG: '"dep"',
-  BASE_API: '"http://localhost:8083/demo/admin"'
+  // BASE_API: '"http://localhost:8083/demo/admin"'
+  BASE_API: '"http://localhost:8528/app/admin"'
 }
